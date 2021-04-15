bd9951a42b7078c04e525025d15380503f0d5a9e
Beyza Parlak <beyza.parlak98@erzurum.edu.tr>
2021-04-15 13:58:23 +0300

    +initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}
